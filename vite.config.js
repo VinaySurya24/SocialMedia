dd055e823cac7174da1084749e450eccd8218e8d
Vinay <vinayswastikh@gmail.com>
2025-01-25 13:34:01 +0530

    Update for static deployment and improved responsive design

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SocialMedia/',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    minify: 'terser',
+    sourcemap: false,
+  },
 })
